afb925cc2269f4351724e2d9168b353060ba249e
raul9001 <raul.ansarov@mail.ru>
2024-05-29 20:48:29 +0400

    header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    < >
+    <Navbar />
+   
+    </>
+  )
+}
+
+export default App
