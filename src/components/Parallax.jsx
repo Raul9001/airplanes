cbc32a667bca57f1752cfefa1ac54e10156f2afb
raul9001 <raul.ansarov@mail.ru>
2024-08-02 03:36:35 +0400

    gjgj

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -12,22 +12,22 @@ const Parallax = () => {
         <div className='container'>
         <div className="stats-section">
         <div className="stats-item">
-            <h2 className='playfair-display-medium mb-2'>95</h2>
+            <h2 className='playfair-display-medium mb-3'>95</h2>
             <p className='mulish-bold'>Professional Pilots</p>
         </div>
         <div className="divider"></div>
         <div className="stats-item">
-            <h2 className='playfair-display-medium mb-2'>68</h2>
+            <h2 className='playfair-display-medium mb-3'>68</h2>
             <p className='mulish-bold'>Jet Airplanes</p>
         </div>
         <div className="divider"></div>
         <div className="stats-item">
-            <h2 className='playfair-display-medium mb-2'>290</h2>
+            <h2 className='playfair-display-medium mb-3'>290</h2>
             <p className='mulish-bold'>World Airports</p>
         </div>
         <div className="divider"></div>
         <div className="stats-item">
-            <h2 className='playfair-display-medium mb-2'>195</h2>
+            <h2 className='playfair-display-medium mb-3'>195</h2>
             <p className='mulish-bold'>Directions</p>
         </div>
     </div>
